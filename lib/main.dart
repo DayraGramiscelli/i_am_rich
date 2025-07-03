d412bc0300ec07a26e87fc14b6ef8e779bb0daa0
DayraGramiscelli <dayra@moonlightmobile.dev>
2025-07-03 09:41:09 -0300

    Image

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Image(
+          image: NetworkImage(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpqCtSbx2P8bAlnZ8T1VG8yDMGDmgrMAYsPRU8ytRA5jsFJQUWXSu1Gt8AO3qp-cP_2Mc&usqp=CAU'),
+          alignment: Alignment.center,
+        ),
       ),
     ),
   );
